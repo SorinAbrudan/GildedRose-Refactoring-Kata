7b453bf786975402c0df9f59bca073519b2a8369
SorinAbrudan <abrudan.sorin90@gmail.com>
2020-11-05 16:59:12 +0200

    Added test, refactored item class and moved item field manipulation there

--- Java/src/main/java/com/gildedrose/Item.java
+++ Java/src/main/java/com/gildedrose/Item.java
@@ -2,11 +2,11 @@ package com.gildedrose;
 
 public class Item {
 
-    public String name;
+    private final String name;
 
-    public int sellIn;
+    private int sellIn;
 
-    public int quality;
+    private int quality;
 
     public Item(String name, int sellIn, int quality) {
         this.name = name;
@@ -14,7 +14,35 @@ public class Item {
         this.quality = quality;
     }
 
-   @Override
+    public String name() {
+        return name;
+    }
+
+    public int quality() {
+        return quality;
+    }
+
+    public int sellIn() {
+        return sellIn;
+    }
+
+    public void decreseSellIn(){
+       sellIn -= 1;
+    }
+
+    public void decreseQuality(){
+        quality -= 1;
+    }
+
+    public void increaseQuality(){
+        quality += 1;
+    }
+
+    public void resetQuality(){
+        quality = 0;
+    }
+
+    @Override
    public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
     }
--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -6,12 +6,46 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
+    private GildedRose gildedRose;
+    private GildedRose newGildedRose;
+
+    void init(){
+        Item[] items = new Item[] {
+                new Item("+5 Dexterity Vest", 10, 20), //
+                new Item("Aged Brie", 2, 0), //
+                new Item("Elixir of the Mongoose", 5, 7), //
+                new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+                new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+                // this conjured item does not work properly yet
+                new Item("Conjured Mana Cake", 3, 6) };
+
+        Item[] newItems = new Item[] {
+                new Item("+5 Dexterity Vest", -19, 0), //
+                new Item("Aged Brie", -27, 50), //
+                new Item("Elixir of the Mongoose", -24, 0), //
+                new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+                new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+                new Item("Backstage passes to a TAFKAL80ETC concert", -14, 0),
+                new Item("Backstage passes to a TAFKAL80ETC concert", -19, 0),
+                new Item("Backstage passes to a TAFKAL80ETC concert", -24, 0),
+                // this conjured item does not work properly yet
+                new Item("Conjured Mana Cake", -26, 0) };
+
+        gildedRose = new GildedRose(items);
+        newGildedRose = new GildedRose(newItems);
+    }
+
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+    void thirtyDaysTest() {
+      init();
+      for (int i=0; i<29; i++) {
+          gildedRose.updateQuality();
+      }
+
+      assertEquals(gildedRose.toString(), newGildedRose.toString());
     }
 
 }
